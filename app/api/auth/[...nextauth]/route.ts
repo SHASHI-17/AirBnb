c0387feb923c5fa8966b452e0f087a8036f06405
SHASHI-17 <hibye31583@gmail.com>
2024-04-23 21:39:51 +0530

    auth options fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,65 +1,73 @@
-// @ts-nocheck
-import type { AuthOptions } from 'next-auth';
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import prisma from '@/app/libs/prismadb'
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import bcrypt from 'bcrypt';
+// // @ts-nocheck
+// import type { AuthOptions } from 'next-auth';
+// import NextAuth from 'next-auth';
+// import GithubProvider from 'next-auth/providers/github'
+// import GoogleProvider from 'next-auth/providers/google'
+// import CredentialsProvider from 'next-auth/providers/credentials'
+// import prisma from '@/app/libs/prismadb'
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
+// import bcrypt from 'bcrypt';
 
- const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-            allowDangerousEmailAccountLinking: true,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            allowDangerousEmailAccountLinking: true,
-        }),
-        CredentialsProvider({
-            name: 'credentials',
-            credentials: {
-                email: { label: 'email', type: 'text' },
-                password: { label: 'password', type: 'password' }
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
-                    throw new Error('Invalid Credentials');
-                }
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email,
-                    }
-                })
+//  const authOptions: AuthOptions = {
+//     adapter: PrismaAdapter(prisma),
+//     providers: [
+//         GithubProvider({
+//             clientId: process.env.GITHUB_ID as string,
+//             clientSecret: process.env.GITHUB_SECRET as string,
+//             allowDangerousEmailAccountLinking: true,
+//         }),
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID as string,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//             allowDangerousEmailAccountLinking: true,
+//         }),
+//         CredentialsProvider({
+//             name: 'credentials',
+//             credentials: {
+//                 email: { label: 'email', type: 'text' },
+//                 password: { label: 'password', type: 'password' }
+//             },
+//             async authorize(credentials) {
+//                 if (!credentials?.email || !credentials?.password) {
+//                     throw new Error('Invalid Credentials');
+//                 }
+//                 const user = await prisma.user.findUnique({
+//                     where: {
+//                         email: credentials.email,
+//                     }
+//                 })
 
-                if (!user || !user?.hashedPassword) {
-                    throw new Error('Invalid Credentials');
-                }
-                const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
+//                 if (!user || !user?.hashedPassword) {
+//                     throw new Error('Invalid Credentials');
+//                 }
+//                 const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
 
-                if (!isCorrectPassword) {
-                    throw new Error('Invalid Credentials');
-                }
+//                 if (!isCorrectPassword) {
+//                     throw new Error('Invalid Credentials');
+//                 }
 
-                return user;
-            },
-        })
-    ],
-    pages: {
-        signIn: '/'
-    },
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: 'jwt'
-    },
-    secret: process.env.NEXTAUTH_SECRET,
+//                 return user;
+//             },
+//         })
+//     ],
+//     pages: {
+//         signIn: '/'
+//     },
+//     debug: process.env.NODE_ENV === 'development',
+//     session: {
+//         strategy: 'jwt'
+//     },
+//     secret: process.env.NEXTAUTH_SECRET,
 
-};
+// };
+
+// const handler = NextAuth(authOptions);
+// export {authOptions, handler as GET, handler as POST };
+
+
+import NextAuth from "next-auth/next";
+import { authOptions } from "../../../libs/authOptions";
 
 const handler = NextAuth(authOptions);
-export {authOptions, handler as GET, handler as POST };
+
+export { handler as GET, handler as POST };
